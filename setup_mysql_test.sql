1073314b49b7db6548ca5f3e71c08a8db12efb43
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-03-19 06:09:07 +0300

    MySQL setup test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,9 @@
+-- prepares a MySQL server by creating database hbnb_test_db
+-- grants all priviledges to user hbnb_test in localhost
+
+CREATE DATBASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEDGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+FLUSH PRIVILEDGES;
