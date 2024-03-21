5605de5bbf61688362c6ca8c7b8fc7fcc84375a8
Jumoke1 <kazeemjumoke12@gmail.com>
2024-03-20 20:19:25 -0700

    city

--- models/city.py
+++ models/city.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel
-
+from sqlalchemy import String, Column, ForeignKey
+from sqlalchemy.orm import relationship
+from models.place import Place
+import os
+from os import getenv
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
+    __tablename__ = 'cities'
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship("Place", cascade='all, delete', backref="cities")
     state_id = ""
-    name = ""
+    name""
--- models/user.py
+++ models/user.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
-
+from sqlalchemy import String, Column
+from sqlalchemy.orm import relationship
+from models.place import Place
+import os
+from os import getenv
 
 class User(BaseModel):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    __tablename__ = "users"
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+         places = relationship("Place", cascade='all, delete', backref="user")
+        reviews = relationship("Review", backref="user")
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
