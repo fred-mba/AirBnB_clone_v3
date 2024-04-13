2353b9a141b0de7bd1c9debbe45cf638410bbabd
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-04-13 18:50:01 +0300

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,16 +2,9 @@
 # sets up your web servers for the deployment of web_static
 
 # Install Nginx if it not already installed
-if ! command -v nginx;
-then
-    echo "Nginx is not installed. Installing..."
-    sudo apt-get update
-    sudo apt-get install nginx -y
-    sudo systemctl start nginx.service
-
-else
-    echo "Nginx is already installed. Skipping installation."
-fi
+sudo apt-get update
+sudo apt-get install nginx -y
+sudo service nginx start
 
 # Create necessary parent directories
 sudo mkdir -p /data/web_static/shared/
@@ -30,5 +23,4 @@ sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '49i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 
 # If no problems were found, restart Nginx to enable your changes
-sudo nginx -t
 sudo systemctl restart nginx
