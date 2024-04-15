7c8a8f27592aaeb0a9e50ce59d135d0de6c9a0da
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-04-15 15:48:11 +0300

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,32 +5,32 @@
 if ! command -v nginx;
 then
     echo "Nginx is not installed. Installing..."
-    sudo apt-get update
-    sudo apt-get install nginx -y
-    sudo service nginx start
+    apt-get update
+    apt-get install nginx -y
+    service nginx start
 
 else
     echo "Nginx is already installed. Skipping installation."
 fi
 
 # Create necessary parent directories
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
 
 # Creating symbolic links
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # create sample pages
 echo "Holberton School" > /data/web_static/releases/test/index.html
 
 # Adjust the permissions or ownership of the folders
-sudo chown -R ubuntu:ubuntu /data/
-sudo chmod -R 755 /data/
+chown -R ubuntu:ubuntu /data/
+chmod -R 755 /data/
 
 # sudo ln -s "/etc/nginx/sites-available/default" "/etc/nginx/sites-enabled/"
 # Creating Server Block Files
-sudo sed -i '49i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+sed -i '49i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 
 # If no problems were found, restart Nginx to enable your changes
-sudo nginx -t
-sudo service nginx restart
+nginx -t
+service nginx restart
