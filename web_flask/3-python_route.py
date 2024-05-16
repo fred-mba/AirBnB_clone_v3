060e0aeb3723b5fc25a7798847404b93d09affd3
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-05-16 18:49:06 +0300

    Python is cool!

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,35 +1,44 @@
-from flask import flask
-""" start flask application
+#!/usr/bin/python3
+""" Start flask application
+    Must be listening on 0.0.0.0, port 5000
 routes:
- /: Displays 'Hello HBNB!'.
+     /: Displays 'Hello HBNB!'.
     /hbnb: Displays 'HBNB'.
-    /c/<text>: Displays 'C' followed by the value of <text>.
-    /python/<text>: display “Python ”, followed by the value of the text
+    /c/<text>: Displays 'C' followed by the value of <text>
+              Replace underscore _ symbols with a space
+    /python/<text>: display “Python ”, followed by the value of <text>
+              Replace underscore _ symbols with a space
+
+The default value of text is “is cool”
 """
+from flask import Flask
+
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    return 'Hello HBNB!'
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return 'HBNB'
+    return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    formatted_text = text.replace('_', ' ')
-    return 'C {}'.format(formatted_text)
+def show_c_text(text):
+    converter = text.replace('_', ' ')
+    return f'C {converter}'
 
 
-@app.route('/python/', strict_slashes=false)
-@app.route('/python/<text>', strict_slashes=false)
-def python_text(test='is cool'):
-    formatted_text = text.replace('_', ' ')
-    return 'python {}'.format(formatted_text)
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def show_python_text(text='is cool'):
+    converter = text.replace('_', ' ')
+    return f'Python {converter}'
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
