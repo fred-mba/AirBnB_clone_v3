68dcf7b6a831c9633a7b7376247b8aba05341121
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-05-16 19:22:55 +0300

    Is it a number?

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,40 +1,50 @@
-from flask import flask
-""" start flask application
+#!/usr/bin/python3
+""" Start flask application
+    Must be listening on 0.0.0.0, port 5000
 routes:
- /: Displays 'Hello HBNB!'.
+     /: Displays 'Hello HBNB!'.
     /hbnb: Displays 'HBNB'.
-    /c/<text>: Displays 'C' followed by the value of <text>.
-    /python/<text>: display “Python ”, followed by the value of the text
+    /c/<text>: Displays 'C' followed by the value of <text>
+              Replace underscore _ symbols with a space
+    /python/<text>: display “Python ”, followed by the value of <text>
+              Replace underscore _ symbols with a space
+    /number/<n>: display “n is a number” only if n is an integer
+
+The default value of text is “is cool”
 """
+from flask import Flask
+
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    return 'Hello HBNB!'
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return 'HBNB'
+    return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    formatted_text = text.replace('_', ' ')
-    return 'C {}'.format(formatted_text)
+def show_c_text(text):
+    converter = text.replace('_', ' ')
+    return f'C {converter}'
 
 
 @app.route('/python/', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=false)
-def python_text(test='is cool'):
-    formatted_text = text.replace('_', ' ')
-    return 'python {}'.format(formatted_text)
+@app.route('/python/<text>', strict_slashes=False)
+def show_python_text(text='is cool'):
+    converter = text.replace('_', ' ')
+    return f'Python {converter}'
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
-    return "{:d} is a number".forman(n)
+def is_a_number(n):
+    return f'{n} is a number'
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
