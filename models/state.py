33f6bd457ae688f50afdc599e933eeb9280745dc
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2025-09-15 00:14:20 +0300

    Added getter property for cities

--- models/state.py
+++ models/state.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import getenv
@@ -11,17 +10,20 @@ class State(BaseModel, Base):
     """Representing the state class"""
     __tablename__ = 'states'
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        id = Column(String(60), primary_key=True, nullable=False)
+        #id = Column(String(60), primary_key=True, nullable=False)
         name = Column(String(128), nullable=False)
         cities = relationship('City', backref='state',
                               cascade='all, delete, delete-orphan')
     else:
+        name = ''
 
         @property
         def cities(self):
             """Returns the list of City instances linked with this State
             """
             from models import storage
+            from models.city import City
+
             city_list = []
             for city in storage.all(City).values():
                 if city.state_id == self.id:
