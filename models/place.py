45f42a18d5e012aa6e889315a0daed6fc86ecb0f
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2025-09-15 00:12:43 +0300

    Added implementation of getters and setters

--- models/place.py
+++ models/place.py
@@ -45,9 +45,25 @@ class Place(BaseModel, Base):
         reviews = relationship("Review", cascade='all, delete', backref="place")
         amenities = relationship("Amenity",
                                  secondary=place_amenity,
-                                 back_populates="place_amenity",
+                                 back_populates="places",
                                  viewonly=False)
     else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+
+        def __init__(self, *args, **kwargs):
+            "Initialize place with amenity_ids"
+            super().__init__(*args, **kwargs)
+            self.amenity_ids = []
+
         @property
         def reviews(self):
             """
@@ -56,21 +72,38 @@ class Place(BaseModel, Base):
              the current Place.id
             """
             from models import storage
+            from models.review import Review
 
             review_list = []
-            for review in self.storage.all(Review).values():
+            for review in storage.all(Review).values():
                 if place_id == self.id:
                     review_list.append(review)
             return review_list
 
-        #city_id = ""
-        #user_id = ""
-        #name = ""
-        #description = ""
-        """number_rooms = 0
-        number_bathrooms = 0
-        max_guest = 0
-        price_by_night = 0
-        latitude = 0.0
-        longitude = 0.0
-        amenity_ids = []"""
+        @property
+        def amenities(self):
+            """
+            Returns the list of Amenity instances based on the attribute
+            amenity_ids that contains all Amenity.id linked to the Place
+            """
+            from models import storage
+            from models.amenity import Amenity
+
+            amenity_list = []
+            for amenity in storage.all(Amenity).values():
+                if amenity.id in self.amenity_ids:
+                    amenity_list.append(amenity)
+            return amenity_list
+
+        @amenities.setter
+        def amenities(obj, Amenity):
+            """
+            - Handles append method for adding an Amenity.id to the attribute
+              amenity_ids.
+            - Should accept only Amenity object, otherwise, do nothing
+            """
+            from models.amenity import Amenity
+
+            if isinstance(obj, Amenity):
+                if obj.id not in self.amenity_ids:
+                    self.amenity_ids.append(obj.id)
