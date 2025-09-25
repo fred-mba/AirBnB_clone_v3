7cba9e1720409b0d0bb1a6d810ad90dfde5d5768
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2025-09-25 23:59:00 +0300

    List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,35 +1,27 @@
 #!/usr/bin/python3
 """List all State objects in DBstorage"""
 
-from flask import Flask, render_template, g
-from models import storage, DBStorage
+from flask import Flask, render_template
+from models import storage
 from models.state import State
 
 
 app = Flask(__name__)
 
 
-def get_db():
-    if "db" not in g:
-        g.db = storage.all()
-    return g.db
-
-
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """List of all State objects present in DBStorage sorted by name (A->Z)"""
-    if hasattr(storage, "_DBStorage__session"):
-        db = get_db()
-        result = db.query(State).order_by(State.name).all()
-    return render_template('7-states_list.html', list_state=result)
+    storage_dict = storage.all(State)
+    states_list = list(storage_dict.values())
+    sorted_state = sorted(states_list, key=lambda state: state.name)
+    return render_template('7-states_list.html', list_state=sorted_state)
 
 
 @app.teardown_appcontext
 def teardown_appctx(exception):
     """Ensures DB session is removed after each request"""
-    db = g.pop("db", None)
-    if db is not None:
-        storage.close()
+    storage.close()
 
 
 if __name__ == '__main__':
