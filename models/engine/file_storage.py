9208f3ddb8499f64340ec7a9114032907b4134c4
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2025-10-11 22:23:24 +0300

    f-strings not supported by python <3.6

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -24,7 +24,7 @@ class FileStorage:
         if obj is None:
             return
 
-        key = f"{obj.__class__.__name__}.{obj.id}"
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
         if key in self.__objects:
             del self.__objects[key]
 
@@ -34,7 +34,7 @@ class FileStorage:
            incorrectly implemented, returning `DeclarativeMeta` as a class.
         """
         if obj is not None and hasattr(obj, "to_dict"):
-            key = f"{obj.__class__.__name__}.{obj.id}"
+            key = f"{}.{}".format(obj.__class__.__name__, obj.id)
             self.__objects[key] = obj
 
     def save(self):
