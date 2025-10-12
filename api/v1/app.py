0f2d3656399a7c080b5a813f6a8c6c5098744e53
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2025-10-12 22:59:11 +0300

    Get environment variables

--- api/v1/app.py
+++ api/v1/app.py
@@ -18,6 +18,6 @@ def tear_appctx(exception):
 
 
 if __name__ == "__main__":
-    host = getenv(HBNB_API_HOST, '0.0.0.0')
-    port = int(getenv(HBNB_API_PORT, '5000'))
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(getenv('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
