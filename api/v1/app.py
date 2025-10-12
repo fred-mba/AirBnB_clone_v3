39c2623b9c6d71e7e1b88730e6c3fd26bc723b08
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2025-10-12 22:53:59 +0300

    Get environment variables

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,7 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
 app = Flask(__name__)
 
@@ -17,4 +18,6 @@ def tear_appctx(exception):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+    host = getenv(HBNB_API_HOST, '0.0.0.0')
+    port = int(getenv(HBNB_API_PORT, '5000'))
+    app.run(host=host, port=port, threaded=True)
