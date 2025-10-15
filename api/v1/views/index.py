4cfaf23133e5f389c2f61ed17e2059b5c37feb3c
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2025-10-15 05:42:56 +0300

    Improved version

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,7 @@
 
 from flask import jsonify
 from api.v1.views import app_views
-from models import storage, Amenity, City, Place, Review, State, User
+from models import storage, dummy_tables
 
 
 @app_views.route('/status', methods=['GET'])
@@ -14,11 +14,9 @@ def status():
 
 @app_views.route('/stats', methods=['GET'])
 def stats():
-    return jsonify({
-        "amenities": storage.count(Amenity),
-        "cities": storage.count(City),
-        "places": storage.count(Place),
-        "reviews": storage.count(Review),
-        "states": storage.count(State),
-        "users": storage.count(User)
-    })
+    counts = {}
+
+    for key, cls_name in dummy_tables.items():
+        counts[key] = storage.count(cls_name)
+
+    return jsonify(counts)
